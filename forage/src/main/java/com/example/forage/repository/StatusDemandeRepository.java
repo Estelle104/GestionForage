a3ca827e0927eb50a8726dafd4516871d19b74be
Randy <randyrobinson3227@gmail.com>
2026-06-01 22:55:54 +0300

    finishing modifiate status

--- forage/src/main/java/com/example/forage/repository/StatusDemandeRepository.java
+++ forage/src/main/java/com/example/forage/repository/StatusDemandeRepository.java
@@ -3,7 +3,9 @@ package com.example.forage.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.forage.entity.StatusDemande;
+import com.example.forage.entity.Demande;
+import java.util.List;
 
 public interface StatusDemandeRepository extends JpaRepository<StatusDemande, Integer> {
-
+    List<StatusDemande> findByDemande(Demande demande);
 }    
